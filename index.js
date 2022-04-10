e691e0e3880a966025349dae41c7f9ec24b7f38d
unallsukru <unallsukru@gmail.com>
2022-04-10 23:29:29 +0300

    Fix at "Divide" function

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 const Sum = (number1, number2) => number1 + number2;
-const Divide = (number1, number2) => number1 / number2;
+const Divide = (number1, number2) => {
+  if (number2 === 0) return;
+
+  return number1 / number2;
+};
